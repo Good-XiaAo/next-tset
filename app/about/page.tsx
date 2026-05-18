314cf2ab4a99d01043121caae9ac493d34911a0f
祥兆 孔 <40678480+Good-XiaAo@users.noreply.github.com>
2026-05-18 08:29:39 +0000

    新增about

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import React, { memo } from "react";
+const About: React.FC = () => {
+  return (
+    <>
+      <h1>About</h1>
+    </>
+  );
+};
+export default memo(About);
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>首页</div>;
+  console.log("Home123");
+  return (
+    <>
+      <h1>Home</h1>
+      <Link href="/about">导航到About</Link>
+    </>
+  );
 }
